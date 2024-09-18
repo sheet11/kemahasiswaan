58666d4f0a1b5d55c015ba4f9775394e8845054e
rey <ahmadreynaldo@gmail.com>
2024-09-18 16:48:49 +0700

    aa

--- administrator/karyawan/01_cetak_ijazah1.php
+++ administrator/karyawan/01_cetak_ijazah1.php
@@ -9,10 +9,10 @@
 
 
 <?php
-    include"../config/koneksi.php";
+    include "../config/koneksi.php";
     include('bar128.php');
-  include("library.php");
-  include("fucnt_tgl.php");
+    include("library.php");
+    include("fucnt_tgl.php");
     
     $query=mysqli_query($kon, "SELECT * from tb_ijazah where id_ijazah='$_GET[id_ijazah]'");
     $a=mysqli_fetch_array($query);
@@ -54,7 +54,7 @@
 </table>
 
 </br>
-<table border="1" align="center" cellpadding="7" >
+<table border="1" align="center" cellpadding="7" style="margin-right: 150px;">
      <tr>
         <td align="center" colspan ="4" style='font-size:14.0pt;line-height:14px;'>Memberikan Ijazah kepada: </td>
        </tr>
@@ -93,37 +93,23 @@
       <td align="center" colspan="4" style='font-size:13.0pt;line-height:16px;'>beserta segala hak, wewenang dan kewajiban yang melekat pada gelar tersebut.</td>
     </tr>
 
-    <tr>
-      <td>&nbsp;</td>
-      <td><b>Nomor Induk Kependudukan (NIK)</b></td>
-      <td><b>:</b></td>
-      <td><b><?php echo "$a[nik]"; ?></b></td>
-    </tr>
-<tr>
-      <td colspan="4">Telah menyelesaikan pendidikan dan dinyatakan <b>LULUS</b> pada <b> Program Studi <?php echo "$a[jurusan]"; ?> Program <?php echo "$a[program_pendidikan]"; ?></b> tanggal <?php echo "$a[tanggal_wisudah]"; ?>  dan berhak menyandang gelar  <b><?php echo "$a[gelar]"; ?> </b>dengan segala hak dan kewajibannya. </td>
-         </tr>
 
 </table>
-
-
-
-<table border="0" width="100%" style='line-height:13px;'>
+<br><br>
+<table border="1" align="center" style='line-height:13px; margin-right: 200px;'>
 
   <tr>
-        <td width="300">&nbsp;</td>
         <td><b>&nbsp;</b></td>
         <td width="170">&nbsp;</td>
-        <td><b>Diterbitkan di Bengkulu,&nbsp;&nbsp;<?php echo "$a[tanggal_cetak]"; ?> </b></td>
+        <td align="center"><b>Bengkulu,&nbsp;&nbsp;<?php echo "$a[tanggal_cetak]"; ?> </b></td>
     </tr>
 
     <tr>
+        <td align="center"><b>Direktur</b></td>
         <td>&nbsp;</td>
-        <td><b>Direktur</b></td>
-        <td>&nbsp;</td>
-        <td><b>Ketua Jurusan  </b></td>
+        <td align="center"><b>Ketua Jurusan</b></td>
     </tr>
     <tr>
-        <td>&nbsp;</td>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
@@ -132,43 +118,35 @@
         <td>&nbsp;</td>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
+    </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
         <td>&nbsp;</td>
     </tr>
     <tr>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
-        <td>&nbsp;</td>
     </tr>
     <tr>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-    </tr>
-    <tr>
-        <td>&nbsp;</td>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
     </tr>
      <tr>
-        <td width="240">&nbsp;</td>
-        <td><b><?php echo "$a[direktur]"; ?></b></td>
+        <td align="center"><?php echo "$a[direktur]"; ?></td>
         <td width="220">&nbsp;</td>
-        <td><b><?php echo "$a[pudir]"; ?></b></td>
+        <td align="center"><?php echo "$a[pudir]"; ?></td>
     </tr>
     <tr>
-        <td>&nbsp;</td>
-        <td><b><?php echo "$a[nip_direktur]"; ?></b></td>
-        <td>&nbsp;</td>
-        <td><b><?php echo "$a[nip_pudir]"; ?></b></td>
+        <td align="center"><?php echo "$a[nip_direktur]"; ?></td>
+        <td align="center">&nbsp;NIK: <?php echo "$a[nik]"; ?></td>
+        <td align="center"><?php echo "$a[nip_pudir]"; ?></td>
     </tr>
 </table>
 
-
-
-  </table>
+</table>
 
 </body>
 </html>
