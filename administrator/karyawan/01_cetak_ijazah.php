5cdd3cb776011ef5d8b048dddaf39fb8767eea27
sheet11 <onlyonesheet@gmail.com>
2024-10-02 11:45:09 +0700

    update

--- administrator/karyawan/01_cetak_ijazah.php
+++ administrator/karyawan/01_cetak_ijazah.php
@@ -9,10 +9,10 @@
 
 
 <?php
-    include"../config/koneksi.php";
+    include "../config/koneksi.php";
     include('bar128.php');
-  include("library.php");
-  include("fucnt_tgl.php");
+    include("library.php");
+    include("fucnt_tgl.php");
     
     $query=mysqli_query($kon, "SELECT * from tb_ijazah where id_ijazah='$_GET[id_ijazah]'");
     $a=mysqli_fetch_array($query);
@@ -34,87 +34,80 @@
     </tr>
   </table>
 
-
 <p>&nbsp;</p><p>&nbsp;</p>
 
-
-<table border="0"  cellpadding="0.1" align="center">
+<table border="0"  cellpadding="0.1" align="center" style="margin-right: 180px;">
+  <tr>
+    <td align="center" colspan="3" style='font-size:20.0pt;line-height:14px;'><b>Politeknik Kesehatan Kementerian Kesehatan Bengkulu</b></td>
+  </tr>
+  <tr><td></td></tr>
+  <tr><td></td></tr>
     <tr>
-    <td colspan="3" align="center"><h2>Politeknik Kesehatan Kementerian Kesehatan Bengkulu</h2></td>
+        <td align="center" colspan="3">SK Akreditasi Perguruan Tinggi: 794/SK/BAN-PT/Ak.PPJ/PT/X/2020</td>
     </tr>
-    <tr>
-        <td>SK Akreditasi Perguruan Tinggi</td>
-        <td>:</td>
-        <td>794/SK/BAN-PT/Ak.PPJ/PT/X/2020</td>
-    </tr>
 
     <tr>
-        <td>SK Akreditasi Program Studi</td>
-        <td>:</td>
-        <td><?php echo "$a[no_akreditasi_prodi]"; ?></td>
+        <td align="center" colspan="3">SK Akreditasi Program Studi: <?php echo "$a[no_akreditasi_prodi]"; ?></td>
     </tr>
 
 </table>
-
-</br>
-<table border="0"  cellpadding="7" width="100%" >
+<br>
+<table border="0" align="center" cellpadding="7" style="margin-right: 140px;">
      <tr>
-        <td width="30%" colspan ="2" style='font-size:14.0pt;line-height:14px;'>Diberikan Kepada </td>
-        <td colspan="2"></td> 
+        <td align="center" colspan ="4" style='font-size:14.0pt;line-height:14px;'>Memberikan Ijazah kepada: </td>
        </tr>
 
     <tr>
-        <td width="5%">&nbsp;</td>
-        <td width="30%" ><b>Nama</b></td>
-        <td width="2%"><b>:</b></td>
-        <td style='font-size:16.0pt;line-height:16px;'><b><?php echo "$a[nama_lengkap]"; ?></b></td>
-    </tr>
-    <tr>
-      <td>&nbsp;</td>
-      <td><b>Tempat, Tanggal Lahir</b></td>
-      <td><b>:</b></td>
-      <td><b><?php echo "$a[tempatdantgl_lahir]"; ?></b></td>
+        <td align="center" colspan="4" style='font-size:16.0pt;line-height:16px;'><b><?php echo "$a[nama_lengkap]"; ?></b></td>
     </tr>
 
     <tr>
-        <td>&nbsp;</td>
-        <td><b>Nomor Induk Mahasiswa (NIM)</b></td>
-        <td><b>:</b></td>
-        <td><b><?php echo "$a[nim]"; ?></b></td>
+        <td align="center" colspan="4" style='font-size:13.0pt;line-height:16px;'>NIM: <?php echo "$a[nim]"; ?></td>
       </td>
     </tr>
+    <tr>
+      <td></td>
+    </tr>
 
     <tr>
-      <td>&nbsp;</td>
-      <td><b>Nomor Induk Kependudukan (NIK)</b></td>
-      <td><b>:</b></td>
-      <td><b><?php echo "$a[nik]"; ?></b></td>
+      <td align="center" colspan="4" style='font-size:13.0pt;line-height:16px;'>Lahir di <?php echo "$a[tempat]"; ?> tanggal <?php echo "$a[tgl_lahir]"; ?></b></td>
     </tr>
-<tr>
-      <td colspan="4">Telah menyelesaikan pendidikan dan dinyatakan <b>LULUS</b> pada <b> Program Studi <?php echo "$a[jurusan]"; ?> Program <?php echo "$a[program_pendidikan]"; ?></b> tanggal <?php echo "$a[tanggal_wisudah]"; ?>  dan berhak menyandang gelar  <b><?php echo "$a[gelar]"; ?> </b>dengan segala hak dan kewajibannya. </td>
-         </tr>
+    <tr>
+      <td align="center" colspan="4" style='font-size:13.0pt;line-height:16px;'>karena telah menyelesaikan pendidikan dan dinyatakan lulus pada tanggal <?php echo "$a[tanggal_wisudah]"; ?> pada</td>
+    </tr>
+
+    <tr>
+      <td align="center" colspan="4" style='font-size:16.0pt;line-height:16px;'><b>Program Studi <?php echo "$a[jurusan]"; ?> Program <?php echo "$a[program_pendidikan]"; ?></b></td>
+    </tr>
+    <tr>
+      <td align="center" colspan="4" style='font-size:13.0pt;line-height:16px;'>dan kepadanya diberikan gelar</td>
+    </tr>
+
+    <tr>
+      <td align="center" colspan="4" style='font-size:13.0pt;line-height:16px;'><b><?php echo "$a[gelar]"; ?></b></td>
+    </tr>
+
+    <tr>
+      <td align="center" colspan="4" style='font-size:13.0pt;line-height:16px;'>beserta segala hak, wewenang dan kewajiban yang melekat pada gelar tersebut.</td>
+    </tr>
+
 
 </table>
-
-
-
-<table border="0" width="100%" style='line-height:13px;'>
+<br><br>
+<table border="0" align="center" style='line-height:13px; margin-right: 130px;'>
 
   <tr>
-        <td width="300">&nbsp;</td>
         <td><b>&nbsp;</b></td>
         <td width="170">&nbsp;</td>
-        <td><b>Diterbitkan di Bengkulu,&nbsp;&nbsp;<?php echo "$a[tanggal_cetak]"; ?> </b></td>
+        <td align="center"><b>Bengkulu,&nbsp;&nbsp;<?php echo "$a[tanggal_cetak]"; ?> </b></td>
     </tr>
 
     <tr>
+        <td align="center"><b>Direktur</b></td>
         <td>&nbsp;</td>
-        <td><b>Direktur</b></td>
-        <td>&nbsp;</td>
-        <td><b>Ketua Jurusan  </b></td>
+        <td align="center"><b>Ketua Jurusan</b></td>
     </tr>
     <tr>
-        <td>&nbsp;</td>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
@@ -123,43 +116,35 @@
         <td>&nbsp;</td>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
+    </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
         <td>&nbsp;</td>
     </tr>
     <tr>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
-        <td>&nbsp;</td>
     </tr>
     <tr>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-    </tr>
-    <tr>
-        <td>&nbsp;</td>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
     </tr>
      <tr>
-        <td width="240">&nbsp;</td>
-        <td><b><?php echo "$a[direktur]"; ?></b></td>
-        <td width="220">&nbsp;</td>
-        <td><b><?php echo "$a[pudir]"; ?></b></td>
+        <td align="center" width="220"><?php echo "$a[direktur]"; ?></td>
+        <td width="260">&nbsp;</td>
+        <td width="240" align="center"><?php echo "$a[pudir]"; ?></td>
     </tr>
     <tr>
-        <td>&nbsp;</td>
-        <td><b><?php echo "$a[nip_direktur]"; ?></b></td>
-        <td>&nbsp;</td>
-        <td><b><?php echo "$a[nip_pudir]"; ?></b></td>
+        <td align="center" ><?php echo "$a[nip_direktur]"; ?></td>
+        <td align="center">&nbsp;NIK: <?php echo "$a[nik]"; ?></td>
+        <td align="center"><?php echo "$a[nip_pudir]"; ?></td>
     </tr>
 </table>
 
-
-
-  </table>
+</table>
 
 </body>
 </html>
